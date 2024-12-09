4b88b1d12c553c43b8728cd6337efaa9f6f8a64f
Prosper-P-Bowman <prosperpbowman@gmail.com>
2024-12-09 12:38:14 -0500

    Question

---
+++ script.js
@@ -0,0 +1,79 @@
+let select1 = document.getElementsByClassName("select1");
+let select2 = document.getElementsByClassName("select2");
+let result = document.getElementsByClassName("select1");
+
+
+    // if(result !== select2){
+    //     console.log('Me');
+    // }else if(result ==== select2){
+    //     console.log('You');
+    // }else{
+    //     console.log('Them');
+    // }
+
+        // function selectTwo(){
+        //     if(select2 != result){
+        //         alert("OH!");
+        //     };
+        // };
+        // selectTwo();
+
+        function selectOne(){
+            if(select1 === result){
+                alert("You are Correct!");
+                };
+        }
+
+        function selectTwo(){
+            if(select2 !== result){
+                alert("You are Wrong!");
+            }else{
+                alert("HMM");
+            }
+        }
+
+        const offAll = document.getElementById("submit");
+        offAll.addEventListener("click",submitOn);
+
+        function submitOn(){
+            
+        }
+
+
+
+    
+    
+
+
+     // function selectOne(){
+        //     const select1 = document.getElementsByClassName("select1");
+        //     const result = document.getElementsByClassName("select1");
+        //     let name = "Prosper";
+        //     if(select1 === result){
+        //         alert("Oh!");
+        //         }else{
+        //             alert("Hmm");
+        //         };
+        // }
+        
+    
+        // function selectTwo(){
+        //     const select2 = document.getElementsByClassName("select2");
+        //     const result = document.getElementsByClassName("select1");
+        //     let name = "Prosper";
+        //     if(select2 !== result){
+        //         alert("Bye!");
+        //         }else{
+        //             alert("By");
+        //         };
+        // }
+
+        // selectOne();
+        // selectTwo();
+
+    
+
+    
+
+
+
