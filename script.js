dc5a6aecf73591c7640fb78d08d625a5aa8d47ba
prosper <prosper@kali.hostle>
2025-03-15 06:42:59 -0500

    Questionaire

--- script.js
+++ script.js
@@ -1,79 +1,24 @@
-let select1 = document.getElementsByClassName("select1");
-let select2 = document.getElementsByClassName("select2");
-let result = document.getElementsByClassName("select1");
+let mybutton = document.getElementById("mybutton");
 
+mybutton.addEventListener("click", function() {
+  let myinput = document.getElementsByTagName("input");
+  let demo = document.getElementsByClassName("demo");
 
-    // if(result !== select2){
-    //     console.log('Me');
-    // }else if(result ==== select2){
-    //     console.log('You');
-    // }else{
-    //     console.log('Them');
-    // }
-
-        // function selectTwo(){
-        //     if(select2 != result){
-        //         alert("OH!");
-        //     };
-        // };
-        // selectTwo();
-
-        function selectOne(){
-            if(select1 === result){
-                alert("You are Correct!");
-                };
-        }
-
-        function selectTwo(){
-            if(select2 !== result){
-                alert("You are Wrong!");
-            }else{
-                alert("HMM");
-            }
-        }
-
-        const offAll = document.getElementById("submit");
-        offAll.addEventListener("click",submitOn);
-
-        function submitOn(){
-            
-        }
-
-
-
-    
-    
-
-
-     // function selectOne(){
-        //     const select1 = document.getElementsByClassName("select1");
-        //     const result = document.getElementsByClassName("select1");
-        //     let name = "Prosper";
-        //     if(select1 === result){
-        //         alert("Oh!");
-        //         }else{
-        //             alert("Hmm");
-        //         };
-        // }
-        
-    
-        // function selectTwo(){
-        //     const select2 = document.getElementsByClassName("select2");
-        //     const result = document.getElementsByClassName("select1");
-        //     let name = "Prosper";
-        //     if(select2 !== result){
-        //         alert("Bye!");
-        //         }else{
-        //             alert("By");
-        //         };
-        // }
-
-        // selectOne();
-        // selectTwo();
-
-    
-
-    
-
-
-
+  for (let i = 0; i < myinput.length; i++) {
+    if (myinput[i].type === "radio" && myinput[i].checked) {
+      
+      if (myinput[i].value != "Picasso") {
+        demo[0].innerHTML = "Incorrect!";
+        demo[0].style.color = "red";
+        demo[0].style.fontWeight = "bold";
+        demo[0].style.fontSize = "20px";
+      };
+      if (myinput[i].value === "Picasso") {
+        demo[0].innerHTML = "Correct!";
+        demo[0].style.color = "green";
+        demo[0].style.fontWeight = "bold";
+        demo[0].style.fontSize = "20px";
+      };
+  }
+}
+});
